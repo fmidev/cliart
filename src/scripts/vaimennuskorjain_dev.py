d1593fd7fc634ec7df57c17d7816c0bd2a5b7e5e
Jussi Tiira <jussi.tiira@fmi.fi>
2023-07-17 16:00:29 +0300

    WIP: initial development

--- src/scripts/vaimennuskorjain_dev.py
+++ src/scripts/vaimennuskorjain_dev.py
@@ -1,15 +1,14 @@
-# SPDX-FileCopyrightText: 2023-present Jussi Tiira <jussi.tiira@fmi.fi>
-#
-# SPDX-License-Identifier: MIT
+import os
 import pyart.correct
 from pyart.correct.attenuation import _param_attzphi_table
 
 from radproc.io import read_h5
 
 
-def correct_attenuation(infile, outfile, band='C'):
-    radar = read_h5(infile)
-    a_coef, beta, c, d = _param_attzphi_table()[band]
+if __name__ == '__main__':
+    fname = os.path.expanduser('~/data/pvol/202208132125_fivih_PVOL.h5')
+    radar = read_h5(fname)
+    a_coef, beta, c, d = _param_attzphi_table()['C']
     attnparams = dict(a_coef=a_coef, beta=beta, c=c, d=d)
     _, _, cor_z, _, _, cor_zdr = pyart.correct.calculate_attenuation_zphi(radar,
         refl_field='reflectivity_horizontal', **attnparams)
--- src/vaimennuskorjain/cli.py
+++ src/vaimennuskorjain/cli.py
@@ -9,9 +9,9 @@ from vaimennuskorjain._version import __version__
 @click.command()
 @click.argument('inputfile', type=click.Path(exists=True, dir_okay=False,
                                              readable=True))
-@click.option('-o', '--outputfile', type=click.Path(dir_okay=False),
+@click.option('-o', '--output-file', type=click.Path(dir_okay=False),
               help='output HDF5 file path',  metavar='PATH')
 @click.version_option(version=__version__, prog_name='vaimennuskorjain')
-def vaimennuskorjain(inputfile, outputfile):
+def vaimennuskorjain(inputfile, output_file):
     """Perform attenuation correction on INPUTFILE."""
     pass
