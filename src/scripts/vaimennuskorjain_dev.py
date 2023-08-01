137dc03c15e81d85b8cc285d8b91c72cb3f65099
Jussi Tiira <jussi.tiira@fmi.fi>
2023-08-01 18:15:47 +0300

    WIP: atten correction smoothing

--- src/scripts/vaimennuskorjain_dev.py
+++ src/scripts/vaimennuskorjain_dev.py
@@ -1,9 +1,11 @@
 import os
 
+import h5py
 import pyart
 import matplotlib.pyplot as plt
+from scipy.ndimage import median_filter
 
-from vaimennuskorjain import correct_attenuation
+from vaimennuskorjain import correct_attenuation, smoothen_atten_cor
 
 
 if __name__ == '__main__':
@@ -11,16 +13,21 @@ if __name__ == '__main__':
     #fname = os.path.expanduser('~/data/pvol/202206030010_fivih_PVOL.h5')
     #fname = os.path.expanduser('~/data/pvol/202307081105_fivih_PVOL.h5')
     #fname = os.path.expanduser('~/data/pvol/202307030835_fivih_PVOL.h5')
-    fname = os.path.expanduser('~/data/pvol/202307010920_fivih_PVOL.h5')
-    radar = correct_attenuation(fname, ml=3500)
-    figz, axz = plt.subplots(nrows=2, ncols=3, figsize=(18, 12), dpi=150)
+    #fname = os.path.expanduser('~/data/pvol/202307010920_fivih_PVOL.h5')
+    fname = os.path.expanduser('~/data/polar/fivan/201708121600_radar.polar.fivan.h5')
+    with h5py.File(fname) as f:
+        ml = f['how'].attrs['freeze']*1000
+    radar = correct_attenuation(fname, ml=ml, smooth_window_len=6)
+    smoothen_atten_cor(radar)
+    figz, axz = plt.subplots(nrows=2, ncols=3, figsize=(18, 12), dpi=120)
     display = pyart.graph.RadarDisplay(radar)
-    display.plot('DBZH', sweep=0, ax=axz[0,0], vmin=0, vmax=40, cmap='viridis')
-    display.plot('DBZHA', sweep=0, ax=axz[0,1], vmin=0, vmax=40, cmap='viridis')
-    #display.plot('SPEC', sweep=0, ax=axz[0,2], vmin=0, vmax=0.5)
-    display.plot('KDP', sweep=0, ax=axz[1,0], vmin=0, vmax=0.3)
-    display.plot('PHIDP', sweep=0, ax=axz[1,1], vmin=0, vmax=150)
-    display.plot('ZDR', sweep=0, ax=axz[1,2], vmin=-1, vmax=2)
+    zmax = 50
+    display.plot('DBZHC', sweep=0, ax=axz[0,0], vmin=0, vmax=zmax, cmap='viridis')
+    display.plot('DBZHA', sweep=0, ax=axz[0,1], vmin=0, vmax=zmax, cmap='viridis')
+    display.plot('DBZHB', sweep=0, ax=axz[0,2], vmin=0, vmax=zmax, cmap='viridis')
+    display.plot('DBZH', sweep=0, ax=axz[1,0], vmin=0, vmax=zmax, cmap='viridis')
+    display.plot('PHIDP', sweep=0, ax=axz[1,1], vmin=0, vmax=350)
+    display.plot('ZDR', sweep=0, ax=axz[1,2], vmin=-3, vmax=2)
     figz.tight_layout()
     #print(radar.fields['SPEC']['data'].max())
     pyart.aux_io.write_odim_h5('/tmp/rad.h5', radar)
--- src/vaimennuskorjain/__init__.py
+++ src/vaimennuskorjain/__init__.py
@@ -3,23 +3,31 @@
 # SPDX-License-Identifier: MIT
 import pyart.correct
 from pyart.correct.attenuation import _param_attzphi_table
+from scipy.signal import savgol_filter
 
 from radproc.io import read_h5
+from radproc.filtering import filter_field
 
 
-def correct_attenuation(infile, ml=3000, band='C'):
+def correct_attenuation(infile, ml=3000, band='C', **kws):
     radar = read_h5(infile, file_field_names=True)
     a_coef, beta, c, d = _param_attzphi_table()[band]
     attnparams = dict(a_coef=a_coef, beta=beta, c=c, d=d)
     namekws = dict(refl_field='DBZH', zdr_field='ZDR', phidp_field='PHIDP')
     spec, pia, cor_z, specd, pida, cor_zdr = pyart.correct.calculate_attenuation_zphi(radar,
-         temp_ref='fixed_fzl', fzl=ml, doc=15, **namekws, **attnparams)
+         temp_ref='fixed_fzl', fzl=ml, doc=20, **namekws, **attnparams, **kws)
     radar.add_field('DBZHA', cor_z)
-    #radar.add_field('PIA', pia)
-    #radar.add_field('SPEC', spec)
+    radar.add_field('PIA', pia)
+    radar.add_field('SPEC', spec)
     radar.add_field('ZDRA', cor_zdr)
     if radar.ray_angle_res is None:
         # TODO: open issue on github
         # TODO: check the correct resolution
         radar.ray_angle_res = {'data': 360/radar.rays_per_sweep['data']}
     return radar
+
+
+def smoothen_atten_cor(radar):
+    filter_field(radar, 'PIA', filterfun=savgol_filter, axis=0, window_length=6, polyorder=3)
+    data = radar.fields['DBZH']['data']+radar.fields['PIA_filtered']['data']
+    radar.add_field_like('DBZHA', 'DBZHB', data=data, replace_existing=True)
