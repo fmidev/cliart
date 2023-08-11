5350d656b471503d45be291d480b5c316a774d37
Jussi Tiira <jussi.tiira@fmi.fi>
2023-08-11 18:06:33 +0300

    phidp_base0

--- src/vaimennuskorjain/__init__.py
+++ src/vaimennuskorjain/__init__.py
@@ -3,19 +3,34 @@
 # SPDX-License-Identifier: MIT
 import h5py
 import pyart.correct
+import numpy as np
 from pyart.correct.attenuation import _param_attzphi_table
 from scipy.signal import savgol_filter
 
 from radproc.io import read_h5
 from radproc.filtering import filter_field
+from vaimennuskorjain._version import __version__
 
 
-def nonmet_filter(radar):
+def phidp_base0(radar):
+    """Add shifted and filtered phidp field."""
+    gf = nonmet_filter(radar, rhohv_min=0.9)
+    phidp_corr = radar.fields['PHIDP']['data'].copy()
+    phidp_valid = phidp_corr.copy()
+    phidp_valid.mask = np.ma.logical_or(gf.gate_excluded, np.isnan(phidp_corr)).filled(True)
+    phidp_base = np.ma.median(phidp_valid.min(axis=1))
+    phidp_corr -= phidp_base
+    mask = np.ma.getmaskarray(phidp_corr)
+    phidp_corr.mask = np.logical_or(mask, phidp_corr < 0).filled(True)
+    radar.add_field_like('PHIDP', 'PHIDPA', phidp_corr, replace_existing=True)
+
+
+def nonmet_filter(radar, rhohv_min=0.8, z_min=0.5):
     gf = pyart.correct.GateFilter(radar)
     gf.exclude_below('DBZH', 10)
     gf.exclude_above('ZDR', 2, op='and')
-    gf.exclude_below('DBZH', 1)
-    gf.exclude_below('RHOHV', 0.8)
+    gf.exclude_below('DBZH', z_min)
+    gf.exclude_below('RHOHV', rhohv_min)
     return gf
 
 
@@ -24,9 +39,10 @@ def correct_attenuation(infile, ml=None, band='C', **kws):
         with h5py.File(infile) as f:
             ml = f['how'].attrs['freeze']*1000
     radar = read_h5(infile, file_field_names=True)
+    phidp_base0(radar)
     a_coef, beta, c, d = _param_attzphi_table()[band]
     attnparams = dict(a_coef=a_coef, beta=beta, c=c, d=d)
-    namekws = dict(refl_field='DBZH', zdr_field='ZDR', phidp_field='PHIDP')
+    namekws = dict(refl_field='DBZH', zdr_field='ZDR', phidp_field='PHIDPA')
     spec, pia, cor_z, specd, pida, cor_zdr = pyart.correct.calculate_attenuation_zphi(radar,
          temp_ref='fixed_fzl', fzl=ml, doc=15, gatefilter=nonmet_filter(radar),
          **namekws, **attnparams, **kws)
